1bfd1a0128762f83240e2c9dc1f2d2469afee023
gurup <gurup@192.168.0.118>
2023-02-09 16:11:36 +0530

    new project.

---
+++ src/test/java/git/swaping.java
@@ -0,0 +1,16 @@
+package git;
+
+public class swaping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int x=45;
+		 int y=23;
+		 int z=x;
+		 x=y; 
+		 y=z;
+		 System.out.println("before  swapping values are.."+x+"  "+y);
+	}
+
+}
